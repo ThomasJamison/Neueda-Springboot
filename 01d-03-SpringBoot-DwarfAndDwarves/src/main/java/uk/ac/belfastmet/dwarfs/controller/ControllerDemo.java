93cbf3aae4075e100090a8bd98a268d2a023d3ee
JAM17149894 <JAM17149894@CLCLRCAC03.belfastmet.int>
2017-11-28 16:25:27 +0000

    Dwarf Website Setting up

---
+++ 01d-03-SpringBoot-DwarfAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/controller/ControllerDemo.java
@@ -0,0 +1,30 @@
+package uk.ac.belfastmet.dwarfs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/disneyrocks")
+public class ControllerDemo
+{
+	@GetMapping("/")
+	public String disney(Model model)
+	{
+		return "Hello World";
+	}
+	
+	@GetMapping("/bingo")
+	public String bingo(Model model)
+	{
+		return "There was a dog in our back yard";
+	}
+	
+	@GetMapping("/princess")
+	public String princess(Model model)
+	{
+		return "Meghan Markle";
+	}
+}
---
+++ 01d-03-SpringBoot-DwarfAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/controller/TolkienController.java
@@ -0,0 +1,9 @@
+package uk.ac.belfastmet.dwarfs.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TolkienController
+{
+
+}
