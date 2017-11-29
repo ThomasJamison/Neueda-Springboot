97facb0f8203490d471b263b5f7cd9a8f5853c14
JAM17149894 <JAM17149894@CLCLRCAC03.belfastmet.int>
2017-11-29 15:30:28 +0000

    Dwarves and Dwarfs site working

--- 01d-03-SpringBoot-DwarfAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/controller/DisneyController.java
+++ 01d-03-SpringBoot-DwarfAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/controller/DisneyController.java
@@ -1,19 +1,50 @@
 package uk.ac.belfastmet.dwarfs.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import uk.ac.belfastmet.dwarfs.domain.Dwarf;
+import uk.ac.belfastmet.dwarfs.service.DwarfService;
+
 @Controller
 @RequestMapping("/")
 public class DisneyController
 {
+	@Autowired
+	private DwarfService dwarfService;
+	
 	@GetMapping("/")
+	public String index(Model model)
+	{
+		model.addAttribute("pageTitle", "Dwarf Emporium");
+		return "index";
+	}
+	
+	@GetMapping("/disney")
 	public String disney(Model model)
 	{
+		model.addAttribute("pageTitle", "Disney Dwarfs");
+		this.dwarfService = new DwarfService();
+		model.addAttribute("dwarfs", this.dwarfService.getDisneyDwarfs());
 		return "disneyPage";
 	}
+	
+	@GetMapping("/tolkien")
+	public String tolkien(Model model)
+	{
+		model.addAttribute("pageTitle", "Tolkien Dwarves");
+
+		this.dwarfService = new DwarfService();
+		model.addAttribute("dwarves", this.dwarfService.getTolkienDwarves());
+		return "tolkienPage";
+	}
+
+
 
 }
