9a2b64caca874803e94288faba431a06a6831a3a
JAM17149894 <JAM17149894@CLCLRCAC03.belfastmet.int>
2017-11-30 12:01:34 +0000

    Buildings Website
    Updated Nav bar fragment to accept parameters so that "nav-item active"
    is set for current page and ONLY the current page

--- 01-04-SpringBoot-Buildings/src/main/java/uk/ac/belfastmet/buildings/controller/BuildingController.java
+++ 01-04-SpringBoot-Buildings/src/main/java/uk/ac/belfastmet/buildings/controller/BuildingController.java
@@ -1,15 +1,12 @@
 package uk.ac.belfastmet.buildings.controller;
 
-import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import uk.ac.belfastmet.buildings.domain.Building;
 import uk.ac.belfastmet.buildings.service.BuildingService;
 
 @Controller
