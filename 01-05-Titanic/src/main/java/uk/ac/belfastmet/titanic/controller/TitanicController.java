5212d3fc53cb58a940b3760d279fee72fc63dd77
JAM17149894 <JAM17149894@CLCLRCAC03.belfastmet.int>
2017-12-11 11:56:01 +0000

    Added some filtered tables

--- 01-05-Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/TitanicController.java
+++ 01-05-Titanic/src/main/java/uk/ac/belfastmet/titanic/controller/TitanicController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import uk.ac.belfastmet.titanic.domain.Passenger;
 import uk.ac.belfastmet.titanic.repositories.PassengerRepository;
 
 @Controller
@@ -37,6 +36,15 @@ public class TitanicController
 		model.addAttribute("pageTitle", "Passengers");
 		model.addAttribute("passengers", passengerRepository.findAll());
 		return "passengersPage";
+	}
+	
+	@GetMapping("/more")
+	public String more(Model model)
+	{
+		model.addAttribute("passengersByAge", passengerRepository.findByAgeLessThan(18));
+		model.addAttribute("passengersSurvived", passengerRepository.findBySurvived(1));
+		model.addAttribute("passengersDied", passengerRepository.findBySurvived(0));
+		return "morePage";
 	}
 
 
--- 01-05-Titanic/src/main/java/uk/ac/belfastmet/titanic/repositories/PassengerRepository.java
+++ 01-05-Titanic/src/main/java/uk/ac/belfastmet/titanic/repositories/PassengerRepository.java
@@ -1,10 +1,13 @@
 package uk.ac.belfastmet.titanic.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import uk.ac.belfastmet.titanic.domain.Passenger;
 
 
-public interface PassengerRepository extends CrudRepository<Passenger, Integer> {
-
+public interface PassengerRepository extends CrudRepository<Passenger, Integer> 
+{
+	Iterable<Passenger> findByAgeLessThan(Integer integer);
+	Iterable<Passenger> findBySurvived(Integer integer);
 }
