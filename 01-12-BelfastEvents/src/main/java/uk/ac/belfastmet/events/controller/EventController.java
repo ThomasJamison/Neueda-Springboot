66d090fd80add7925c8501771ab430f80c16ba18
JAM17149894 <JAM17149894@CLCLRCAC03.belfastmet.int>
2017-12-19 13:56:48 +0000

    Added individual pages to events site

--- 01-12-BelfastEvents/src/main/java/uk/ac/belfastmet/events/controller/EventController.java
+++ 01-12-BelfastEvents/src/main/java/uk/ac/belfastmet/events/controller/EventController.java
@@ -4,10 +4,12 @@ package uk.ac.belfastmet.events.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import uk.ac.belfastmet.events.domain.BelfastEvents;
+import uk.ac.belfastmet.events.domain.Event;
 
 
 
@@ -35,6 +37,30 @@ public class EventController
 		model.addAttribute("events", belfastEvents.getAllEvents());
 		
 		return "events";
+	}
+	
+	@GetMapping("/{eventId}")
+	public String singleEvent(Model model, @PathVariable("eventId") String eventId)
+	{
+		model.addAttribute("pageTitle", "Event");
+		
+		
+		String belfastEventsUrl = "http://neueda-flask.bndouglas.c9users.io/belfast-events/api/";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		BelfastEvents belfastEvents = restTemplate.getForObject(belfastEventsUrl, BelfastEvents.class);
+		Event requestedEvent = new Event();
+		
+		for(Event event: belfastEvents.getAllEvents())
+		{
+			if (event.getIdentifier().equals(eventId))
+			{
+				requestedEvent = event;
+			}
+
+		}
+		model.addAttribute("event", requestedEvent);
+		return "event";
 	}
 	
 	
